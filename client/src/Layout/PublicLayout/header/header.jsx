d453b96455c7c427fd6b232900eb01a582a7918b
hoangphuc_0062 <bmtck0000@gmail.com>
2024-08-29 17:02:25 +0700

    ultils + icons

--- client/src/Layout/PublicLayout/header/header.jsx
+++ client/src/Layout/PublicLayout/header/header.jsx
@@ -1,8 +1,43 @@
+import icons from "../../../ultils/icon";
+
+const { AiOutlineSearch, AiOutlineHeart, AiOutlineShoppingCart } = icons;
 
 function Header() {
   return (
-    <div>header</div>
-  )
+    <header class="flex items-center justify-between p-4 bg-background">
+      <h1 class="text-2xl font-bold text-primary">Voi Tây Nguyên</h1>
+      <nav class="flex space-x-4">
+        <a href="#" class="text-dark hover:text-muted-foreground">
+          Trang chủ
+        </a>
+        <a href="#" class="text-dark hover:text-muted-foreground">
+          Sản phẩm
+        </a>
+        <a href="#" class="text-dark hover:text-muted-foreground">
+          Về chúng tôi
+        </a>
+        <a href="#" class="text-dark hover:text-muted-foreground">
+          Đăng nhập / Đăng ký
+        </a>
+      </nav>
+      <div class="flex items-center space-x-2 border-border">
+        <input
+          type="text"
+          placeholder="What are you looking for?"
+          class="p-2 border-none focus:outline-none rounded-md bg-input text-muted-foreground"
+        />
+        <button class="p-2 text-muted hover:text-muted-foreground">
+          <AiOutlineSearch />
+        </button>
+        <button class="p-2 text-muted hover:text-muted-foreground">
+          <AiOutlineHeart />
+        </button>
+        <button class="p-2 text-muted hover:text-muted-foreground">
+          <AiOutlineShoppingCart />
+        </button>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
