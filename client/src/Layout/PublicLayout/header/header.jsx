bdbcfa45dd5a140d0f8f07316bc5271f48561f63
hoangphuc_0062 <bmtck0000@gmail.com>
2024-08-30 16:55:06 +0700

    contact component

--- client/src/Layout/PublicLayout/header/header.jsx
+++ client/src/Layout/PublicLayout/header/header.jsx
@@ -1,27 +1,7 @@
-import icons from "../../../ultils/icon";
-import { SearchInput } from "../../../components/Input/SearchInput";
-
-const { AiOutlineSearch, AiOutlineHeart, AiOutlineShoppingCart } = icons;
+import Navbar from "../../../components/Navbar/Navbar";
 
 function Header() {
-  return (
-    <header class="flex items-center justify-between p-4 bg-background">
-      <h1 class="text-2xl font-bold text-primary">Voi Tây Nguyên</h1>
-      {/* <nav class="flex space-x-4">
-        <a href="#" class="text-dark hover:text-muted-foreground">
-          Trang chủ
-        </a>
-        <a href="#" class="text-dark hover:text-muted-foreground">
-          Sản phẩm
-        </a>
-        <a href="#" class="text-dark hover:text-muted-foreground">
-          Về chúng tôi
-        </a>
-      </nav> */}
-
-      <SearchInput />
-    </header>
-  );
+  return <Navbar />;
 }
 
 export default Header;
--- client/src/ultils/icon.jsx
+++ client/src/ultils/icon.jsx
@@ -1,11 +1,18 @@
 import { AiOutlineSearch } from "react-icons/ai";
 import { AiOutlineHeart } from "react-icons/ai";
 import { AiOutlineShoppingCart } from "react-icons/ai";
-
+import { AiOutlineUnorderedList } from "react-icons/ai";
+import { AiOutlinePhone } from "react-icons/ai";
+import { BsGeoAlt } from "react-icons/bs";
+import { BsTruck } from "react-icons/bs";
 const icons = {
-  AiOutlineSearch,
-  AiOutlineHeart,
-  AiOutlineShoppingCart,
+  AiOutlineSearch, // kinh lup
+  AiOutlineHeart, // yeu thich
+  AiOutlineShoppingCart, // cart
+  AiOutlineUnorderedList, // list
+  AiOutlinePhone, // phone
+  BsGeoAlt, // vị trí
+  BsTruck,
 };
 
 export default icons;
