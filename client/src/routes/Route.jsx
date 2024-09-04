9ff4e3f47d6be2301497d5caf0f654afc4f993a9
racingboi <truongduc2004ok@gmail.com>
2024-09-04 11:04:29 +0700

    Breadcrumbs

--- client/src/routes/Route.jsx
+++ client/src/routes/Route.jsx
@@ -1,30 +1,28 @@
-import { useRoutes } from 'react-router-dom';
+import { useRoutes } from "react-router-dom";
 
-
-import DashboardPage from '../pages/admin/home';
-import HomePage from '../pages/web/home';
-import DashboardLayout from '../Layout/DashboardLayout/Index';
-import PublicLayout from '../Layout/PublicLayout/Index';
-import CategoryList from '../pages/admin/category';
-
+import DashboardPage from "../pages/admin/home";
+import HomePage from "../pages/web/home";
+import DashboardLayout from "../Layout/DashboardLayout/Index";
+import PublicLayout from "../Layout/PublicLayout/Index";
+import CategoryList from "../pages/admin/category";
 export default function RootRouter() {
   const routes = useRoutes([
-     {
-      path: '/',
-      element: <PublicLayout />, 
-      children: [
-        { path: '/', element: <HomePage /> }, 
-      ],
+    {
+      path: "/",
+      element: <PublicLayout />,
+      children: [{ path: "/", element: <HomePage /> }],
     },
     {
-      path: '/admin',
-      element: <DashboardLayout />, 
+      path: "/admin",
+      element: <DashboardLayout />,
       children: [
-        { path: '', element: <DashboardPage /> }, 
-        { path: 'caterory/list', element: <CategoryList /> }, 
+        { path: "", element: <DashboardPage /> },
+        { path: "category", element: <CategoryList /> },
+        { path: "category/create", element: <CategoryList /> },
       ],
     },
   ]);
 
-  return routes;  
+  // return routes;
+  return <div>{routes}</div>;
 }
