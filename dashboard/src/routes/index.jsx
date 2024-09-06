bff9ad7d42f47969188c01a01c775bae7a305e0c
racingboi <truongduc2004ok@gmail.com>
2024-09-06 13:38:10 +0700

    comfig filebase

--- dashboard/src/routes/index.jsx
+++ dashboard/src/routes/index.jsx
@@ -3,6 +3,7 @@ import AdminLayout from "../layouts";
 import HomePage from "../pages/home";
 import CategoryPage from "../pages/category";
 import AddCategory from "../pages/category/create";
+import UploadImage from "../pages/TestFilebase";
 
 export default function RootRouter() {
   const routes = useRoutes([
@@ -13,6 +14,7 @@ export default function RootRouter() {
         { index: true, element: <HomePage /> }, //
         { path: "category", element: <CategoryPage /> },
         { path: "category/create", element: <AddCategory /> },
+        { path: "test", element: <UploadImage /> },
       ],
     },
   ]);
