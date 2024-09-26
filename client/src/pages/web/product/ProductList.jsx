aae1009cda4fe0645df36934b46d52bf941d511b
Phạm Vinh Quang <139889577+vinhquang1404@users.noreply.github.com>
2024-09-26 12:32:41 +0700

    FE add Post and Comment

--- client/src/pages/web/product/ProductList.jsx
+++ client/src/pages/web/product/ProductList.jsx
@@ -3,12 +3,14 @@ import BrandPhone from "../../../components/Brand/BrandPhone";
 import CategorySelector from "../../../components/Card/CategorySelector";
 import { ProductCard } from "../../../components/Card/ProductCard";
 import SortOptions from "../../../components/Card/SortOptions";
+import QnASection from "../../../components/Form/QnAForm";
+import PostTag from "../../../components/Forum/PostTag";
 import Heading from "../../../components/Heading/Heading";
 import CriteriaSelector from "../../../components/Products/CriteriaSelector";
 
 const ProductList = () => {
   return (
-    <div className="container w-full">
+    <div className="container w-full flex flex-col justify-center">
       <section className="flex flex-wrap p-2">
         <div className="w-1/2 p-2">
           <ProductSlider />
@@ -25,7 +27,7 @@ const ProductList = () => {
         <CategorySelector />
       </section>
       <section className="w-full p-2">
-        <Heading title="Lựa Chọn Theo Tiêu Chí" />
+        <Heading title="Chọn Theo Tiêu Chí" />
         <CriteriaSelector />
       </section>
       <section className="w-full p-2">
@@ -34,6 +36,17 @@ const ProductList = () => {
       </section>
       <section className="w-full p-2">
         <ProductCard />
+      </section>
+      <section className="w-full p-2">
+        <Heading title="Tin tức về sản phẩm" />
+        <div className="flex gap-2 overflow-x-auto pt-2">
+          <PostTag category="Trên Tay" />
+          <PostTag category="Tin Công Nghệ" />
+          <PostTag category="Đánh Giá" />
+        </div>
+      </section>
+      <section className="p-2 w-[90%]">
+        <QnASection />
       </section>
     </div>
   );
