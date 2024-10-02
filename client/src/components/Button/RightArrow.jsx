47a6b30307f0b19bb3a248e57f4584ecfa2d6280
Pham Vinh Quang <phamvinhquang14042003@gmail.com>
2024-10-02 11:36:04 +0700

    Update RightArrow.jsx

--- client/src/components/Button/RightArrow.jsx
+++ client/src/components/Button/RightArrow.jsx
@@ -4,6 +4,7 @@ const RightArrow = ({ handleOnClick, disabled }) => {
   return (
     <button
       onClick={handleOnClick}
+      className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-200 rounded-s-full p-4"
       disabled={disabled}
     >
       <IoIosArrowForward />
