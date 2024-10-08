2062a6e147410aeb8c199a0b52f70441086cbc78
Nguyen Duong Hoang Phuc <bmtck0000@gmail.com>
2024-10-08 15:13:29 +0700

    delete many

--- server/routes/postRoute.js
+++ server/routes/postRoute.js
@@ -8,11 +8,10 @@ const { verifyAccessToken, isAdmin } = require("../middlewares/vertifyToken");
 router.get("/", ctrl.getAllPost);
 router.get("/:slug", ctrl.getPostBySlug);
 // apis for admin
-
 router.use([verifyAccessToken, isAdmin]);
 router.post("/create", ctrl.addPost);
 router.get("/id/:bid", ctrl.getPostById);
 router.put("/:bid", ctrl.updatePost);
 router.delete("/:bid", ctrl.deletePost);
-
+router.delete("/", ctrl.deleteManyPost);
 module.exports = router;
