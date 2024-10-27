bc77f269b2d42c59e3989934744edb9ef58c2e82
Nguyen Duong Hoang Phucs <bmtck0000@gmail.com>
2024-10-27 19:18:01 +0700

    Update Account.jsx

--- client/src/pages/web/member/Account.jsx
+++ client/src/pages/web/member/Account.jsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Input } from "../../../components/Input/Input";
 import { Select } from "../../../components/Input/Select";
 import { formatCurrency, convertToISODateString } from "../../../ultils/helper";
+import AvatarEditor from "react-avatar-edit";
+import { Icon } from "@iconify-icon/react/dist/iconify.mjs";
 
 const initialCustomerData = {
   name: "Nguyen Van A",
@@ -14,11 +16,14 @@ const initialCustomerData = {
   totalPurchasePrice: 1000000,
   address: "Ha Noi",
   avatar:
-    "https://res.cloudinary.com/dgthe0zuj/image/upload/fl_preserve_transparency/v1717730182/0d64989794b1a4c9d89bff571d3d5842_xytb2b.jpg?_s=public-apps",
+    "https://scontent.fsgn2-11.fna.fbcdn.net/v/t39.30808-6/449064390_2414483542094432_6673220414551968639_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=RL1utjliNoAQ7kNvgHfL7Ha&_nc_zt=23&_nc_ht=scontent.fsgn2-11.fna&_nc_gid=ASknwc48-eYa-gCpFeRmDPG&oh=00_AYBGm9yS3VKoqWJi5i9kqvsRa0yZqnxQrBvcl3rMf9_aeA&oe=6723C9DB",
 };
 
 export const Account = () => {
   const [customerData, setCustomerData] = useState(initialCustomerData);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [isAvatarVisible, setIsAvatarVisible] = useState(false);
+  const fileInputRef = useRef(null);
 
   const extractCustomerData = (data) => {
     let { name, birthday, sex, email } = data;
@@ -42,6 +47,35 @@ export const Account = () => {
     console.log(filteredData);
   };
 
+  const renderAvatar = () => {
+    if (!isAvatarVisible) return null;
+    const avatarSrc = selectedFile
+      ? URL.createObjectURL(selectedFile)
+      : customerData.avatar;
+
+    return (
+      <div className="text-center font-bold">
+        <h2 className="mx-2">Ảnh tải lên</h2>
+        <img
+          src={avatarSrc}
+          alt={customerData.name}
+          className="w-32 h-32 object-cover rounded-full mx-auto p-2 outline outline-main"
+        />
+      </div>
+    );
+  };
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+      setIsAvatarVisible(true);
+      // console.log("Selected file:", file);
+    }
+  };
+  const handleClick = () => {
+    fileInputRef.current.click();
+  };
+
   return (
     <div className="">
       <div className="space-y-4 mx-auto">
@@ -49,11 +83,29 @@ export const Account = () => {
           Thông tin cá nhân
         </h2>
         <form className="flex flex-col gap-5">
-          <img
-            src={customerData.avatar}
-            alt={customerData.name}
-            className="w-24 h-24 object-cover rounded-full mx-auto"
-          />
+          <div className="flex flex-col justify-center items-center">
+            <div className="flex justify-center w-fit rounded-full relative">
+              <img
+                src={customerData.avatar}
+                alt={customerData.name}
+                className="w-32 h-32 object-cover rounded-full  mx-auto p-2"
+              />
+              <span
+                className="absolute bottom-0 right-0 bg-white rounded-full h-10 w-10 p-2 cursor-pointer"
+                onClick={handleClick}
+              >
+                <Icon icon="mdi:camera" width="1.5rem" height="1.5rem" />
+              </span>
+
+              <input
+                type="file"
+                ref={fileInputRef}
+                style={{ display: "none" }}
+                onChange={handleFileUpload}
+              />
+            </div>
+            {renderAvatar()}
+          </div>
           <Input
             label={"Họ và tên"}
             type="text"
